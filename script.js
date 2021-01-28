7092dfeade8dcf8678f09aee587c7c009f51de2e
SobyDamn <sobydanny@gmail.com>
2021-01-28 21:59:16 +0530

    added -header -footer -print button

---
+++ script.js
@@ -0,0 +1,11 @@
+function printDiv(divName=null){
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+
+}
